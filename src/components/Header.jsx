bc3103732f1394fab756de2b1943ba3d43f3efbd
Ife <taiwoifeoluwa023@gmail.com>
2023-08-08 08:54:39 +0100

    attempt to fix globe icon not showing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import image from '/images/globe-icon.png'
+
 const Header = () => {
     return (
         <header className="header">
-            <img src="../images/globe-icon.png" alt="Image logo" className="logo" />
+            <img src={image} alt="Image logo" className="logo" />
             <p>my travel journal.</p>
         </header>
     )
